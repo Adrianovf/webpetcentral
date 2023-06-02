f15732adcc96060ed1dfc49d0b0bcfb2a199cd26
Adrianovf <adrikovisky@gmail.com>
2023-06-02 01:25:29 -0300

    alteração no php e novo arquivo de texto

--- cliente.php
+++ cliente.php
@@ -1,43 +1,42 @@
 <?php
-    include ('../conexao.php');
-    include ('alertasC.php');
+    include('conexao.php');
+    include('alertasC.php');
 
-    if (isset($_POST['nome'])) $nome=$_POST['nome'];
-    if (isset($_POST['sobrenome'])) $nome=$_POST['sobrenome'];
-    if (isset($_POST['telefone'])) $telefone=$_POST['telefone'];
-    if (isset($_POST['cpf'])) $nome=$_POST['cpf'];
-    if (isset($_POST['email'])) $email=$_POST['email'];
-    if (isset($_POST['senha'])) $nome=$_POST['senha'];
+    $nome = isset($_POST['nome']) ? $_POST['nome'] : '';
+    $sobrenome = isset($_POST['sobrenome']) ? $_POST['sobrenome'] : '';
+    $telefone = isset($_POST['telefone']) ? $_POST['telefone'] : '';
+    $cpf = isset($_POST['cpf']) ? $_POST['cpf'] : '';
+    $email = isset($_POST['email']) ? $_POST['email'] : '';
+    $senha = isset($_POST['senha']) ? $_POST['senha'] : '';
 
-    if (isset($_POST['set'])) $set=$_POST['set'];
-    if (isset($_GET['set'])) $set=$_GET['set'];
+    $set = isset($_POST['set']) ? $_POST['set'] : (isset($_GET['set']) ? $_GET['set'] : '');
 
-    if ($set=='ad') {
+    if ($set == 'ad') {
 
         $pdo_insere = $conexao_pdo->prepare('INSERT INTO cliente
         (nome, sobrenome, telefone, cpf, email, senha)
         VALUES (?, ?, ?, ?, ?, ?)');
-        $pdo_insere->execute( array( $nome, ,$sobrenome, $telefone, $cpf, $email, $senha));
+        $pdo_insere->execute(array($nome, $sobrenome, $telefone, $cpf, $email, $senha));
 
         sets();
 
-    }elseif($set=='up') {
+    } elseif ($set == 'up') {
 
         //Executa alteração no registro do cliente
         $pdo_insere = $conexao_pdo->prepare("UPDATE cliente
                                              SET nome=?, sobrenome=?, telefone=?, email=?, senha=?
                                              WHERE cpf=?");
 
-        $pdo_insere -> execute( array($nome, $sobrenome, $telefone, $cpf, $email, $senha) );
+        $pdo_insere->execute(array($nome, $sobrenome, $telefone, $email, $senha, $cpf));
 
         sets();
-    } elseif ($set=='del') {
+    } elseif ($set == 'del') {
 
         // Exclui o registro do cliente
         $pdo_delete = $conexao_pdo->prepare("DELETE FROM cliente
-                                             WHERE cpf='$cpf'");
+                                             WHERE cpf=?");
 
-        $pdo_delete -> execute();
+        $pdo_delete->execute(array($cpf));
         del();
     }
 ?>
--- verCliente.php
+++ verCliente.php
@@ -1,7 +1,7 @@
 <html>
 <head>
     <meta charset="UTF-8">
-    <title>Sistema de Cadastro</title>
+    <title>Sistema de Consulta</title>
 </head>
 <?php
     $busca = '';
@@ -9,14 +9,12 @@
     include ('conexao.php');
 ?>
 <body>
-    <h1>CADASTRO DE CLIENTES</h1>
-    <br>
-    <h3><a href='../inicio.php' title='voltar'>Voltar ao início</a></h3>
+    <h1>CONSULTA DE CLIENTES</h1>
     <br>
     <form name="form1" method="GET" action="verCliente.php">
         <div>
             <p>
-                <label>Busca Cliente por Código ou pelo Nome:</label>
+                <label>Busca Cliente por Cpf ou pelo Nome:</label>
                 <input name="busca" type="text" autofocus/>
 
                 <label>&nbsp</label>
@@ -29,43 +27,48 @@
     <br><br>
     <table width='100%'>
         <tr bgcolor='fff000'>
-            <td width='10%'><b>Codigo</b></td>
             <td width='30%'><b>Nome</td>
+            <td width='20%'><b>Sobrenome</td>
             <td width='20%'><b>Telefone</td>
-            <td width='20%'><b>E-mail</td>
-            <td width='20%'><b>Ações</td>
+            <td width='10%'><b>Cpf</b></td>
+            <td width='20%'><b>Email</td>
+            <td width='20%'><b>Senha</td>
         </tr>
         <?php
             //Inclui o arquivo de configuração
             include('conexao.php');
 
             //verifica se o usuário existe
-            $pdo_verifica = $conexao_pdo->prepare("SELECT * FROM clientes WHERE Nome_Cliente LIKE '$busca%' OR Cod_Cliente='$busca'");
+            $pdo_verifica = $conexao_pdo->prepare("SELECT * FROM cliente WHERE nome LIKE '$busca%' OR cpf='$busca'");
 
             $pdo_verifica->execute();
 
             //captura os dados da linha
-            while ($rs=$pdo_verifica->fetch()){
-                $codigo     = $rs['Cod_Cliente'];
-                $nome       = $rs['Nome_Cliente'];
-                $telefone   = $rs['Tel_Cliente'];
-                $Email      = $rs['Email_Cliente'];
-        ?>
-        <tr>
-            <td width='10%'><b><?php echo $codigo;?></b></td>
-            <td width='30%'><b><?php echo $nome;?></td>
-            <td width='20%'><b><?php echo $telefone;?></td>
-            <td width='20%'><b><?php echo $Email;?></td>
-            <td width='20%'>
-                <center>
-                    <a href='upCliente.php?Cod_Cliente=<?php echo $codigo;?>' title='Editar'><img src='img/editar.png' width="20" height="20"></a>
-			        <a href='action/cliente.php?Cod_Cliente=<?php echo $codigo;?>&set=del'title='Excluir'><img src='img/deletar.png' width='20' height='20'></a>
-                </center>
-            </td>
-        </tr>
-        <?PHP
+            while ($rs = $pdo_verifica->fetch()) {
+                $nome       = isset($rs['nome']) ? $rs['nome'] : '';
+                $sobrenome  = isset($rs['sobrenome']) ? $rs['sobrenome'] : '';
+                $telefone   = isset($rs['telefone']) ? $rs['telefone'] : '';
+                $cpf        = isset($rs['cpf']) ? $rs['cpf'] : '';
+                $email      = isset($rs['email']) ? $rs['email'] : '';
+                $senha      = isset($rs['senha']) ? $rs['senha'] : '';
+            ?>
+            <tr>
+                <td width='30%'><b><?php echo $nome; ?></td>
+                <td width='10%'><b><?php echo $sobrenome; ?></b></td>
+                <td width='20%'><b><?php echo $telefone; ?></td>
+                <td width='10%'><b><?php echo $cpf; ?></b></td>
+                <td width='20%'><b><?php echo $email; ?></td>
+                <td width='10%'><b><?php echo $senha; ?></b></td>
+                <td width='20%'>
+                    <center>
+                        <a href='upCliente.php?Cod_Cliente=<?php echo $codigo;?>' title='Editar'><img src='img/editar.png' width="20" height="20"></a>
+                        <a href='cliente.php?Cod_Cliente=<?php echo $codigo;?>&set=del' title='Excluir'><img src='img/deletar.png' width='20' height='20'></a>
+                    </center>
+                </td>
+            </tr>
+            <?php
             }
-        ?>
+            ?>
     </table>
 </body>
 </html>
